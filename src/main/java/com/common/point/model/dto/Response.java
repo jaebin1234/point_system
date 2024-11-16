5607ff6bbf30ed667d1af96f773202606dccd851
jaebin1234 <jaebin1234>
2024-11-16 19:34:32 +0900

    feat:공통 응답 빌더패턴 추가

--- src/main/java/com/common/point/model/dto/Response.java
+++ src/main/java/com/common/point/model/dto/Response.java
@@ -1,12 +1,14 @@
-package com.common.point.dto;
+package com.common.point.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 //@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
 
