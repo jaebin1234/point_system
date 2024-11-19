508b89d5c6ae670ef5fbdce64393e650c7d5dc89
jaebin1234 <jaebin1234>
2024-11-19 18:24:24 +0900

    add:Custom Exception ErrorCode 추가

--- src/main/java/com/common/point/exception/ErrorCode.java
+++ src/main/java/com/common/point/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum ErrorCode {
-    BAD_REQUEST_COMPANY_PARAM(400, "Bad Request");
+    BAD_REQUEST_REQUEST_PARAM(400, "Bad Request Parameter"),
+    SERVER_ERROR(500, "Server Error");
 
     private final int status;
     private final String message;
