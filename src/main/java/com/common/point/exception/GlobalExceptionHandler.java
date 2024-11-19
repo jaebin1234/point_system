5a52f1a3fa0ecf338a2bb469f731dc647ead3f4e
jaebin1234 <jaebin1234>
2024-11-19 18:40:49 +0900

    add:validException 추가

--- src/main/java/com/common/point/exception/GlobalExceptionHandler.java
+++ src/main/java/com/common/point/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.common.point.model.dto.ErrorResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,9 +17,14 @@ public class GlobalExceptionHandler {
 		return CustomExceptionReturn.returnException(e);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleValidationExceptions(MethodArgumentNotValidException e) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(400, e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponseDTO> handleGeneralException(Exception e) {
-		// 500 Internal Server Error
 		ErrorResponseDTO errorResponse = new ErrorResponseDTO(500, e.getMessage());
 		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
