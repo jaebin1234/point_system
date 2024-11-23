db70fbca6b0220c93ed10cfbbc2e5b207b68a4ad
jaebin1234 <jaebin1234>
2024-11-23 21:53:20 +0900

    test:feat:여러 회사에 포인트 충전 테스트

--- src/main/java/com/common/point/dao/mapper/UserMapper.java
+++ src/main/java/com/common/point/dao/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package com.common.point.dao.mapper;
 
+import com.common.point.model.dto.PUser;
 import com.common.point.model.vo.PUserVo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
+	List<PUser> selectPUserList();
 	int insertPUser(PUserVo user);
 	void deleteAllUsers();
 }
--- src/test/java/com/common/point/controller/TestPointChargeController.java
+++ src/test/java/com/common/point/controller/TestPointChargeController.java
@@ -1,6 +1,9 @@
 package com.common.point.controller;
 
+import com.common.point.dao.mapper.CompanyMapper;
+import com.common.point.dao.mapper.UserMapper;
 import com.common.point.model.dto.Company;
+import com.common.point.model.dto.PUser;
 import com.common.point.model.dto.PointChargeRequest;
 import com.common.point.model.dto.PointChargeResponse;
 import com.common.point.service.PointChargeService;
@@ -14,6 +17,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+import java.util.Random;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,6 +41,48 @@ public class TestPointChargeController {
 
 //	@MockBean
 //	private PointChargeService pointChargeService;
+
+	@Autowired
+	private CompanyMapper companyMapper;
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+    public void testRandomPointCharge() throws Exception {
+		final Random random = new Random();
+
+		final List<Company> companyList = companyMapper.selectCompanyList();
+		final List<PUser> pUserList = userMapper.selectPUserList();
+
+        for (int i = 0; i < companyList.size(); i++) {
+            for (int j = 0; j < pUserList.size(); j++) {
+                boolean isPaid = random.nextBoolean(); // 유상/무상 포인트 결정
+                int randomPoint = (random.nextInt(30) + 1) * 100;
+
+                PointChargeRequest pointChargeRequest = PointChargeRequest.builder()
+                        .companyNo(companyList.get(i).getCompanyNo())
+                        .userNo(pUserList.get(j).getUserNo())
+                        .point(randomPoint)
+                        .pointType(isPaid ? "C" : "E") // C: 유상, E: 무상
+                        .pointActionType("A") // A: 충전
+                        .description(isPaid ? "유상 포인트 충전" : "무상 포인트 충전")
+                        .build();
+
+                String requestBody = objectMapper.writeValueAsString(pointChargeRequest);
+
+                ResultActions perform = mockMvc.perform(post("/point/charge/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .accept(MediaType.APPLICATION_JSON)
+                );
+
+                perform.andExpect(status().isOk())
+                        .andDo(print());
+            }
+        }
+    }
+
 
 	@Test
 	public void testPaidPointCharge() throws Exception {
