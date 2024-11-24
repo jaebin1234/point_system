201b2797c8f156e220509f9c47e4188608209345
jaebin1234 <jaebin1234>
2024-11-24 15:29:51 +0900

    fix:ErrorCode 추가 및 수정

--- src/main/java/com/common/point/exception/ErrorCode.java
+++ src/main/java/com/common/point/exception/ErrorCode.java
@@ -9,9 +9,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum ErrorCode {
-    BAD_REQUEST_REQUEST_PARAM(400, "Bad Request Parameter"),
-    NO_POINT_LEFT(400, "There are no points left."),
+    BAD_REQUEST_PARAM(400, "Bad Request Parameter"),
     NOT_ENOUGH_POINTS(400, "not enough points."),
+    NOT_EXISTS_COMPANY_POINTS(400, "not exists company points."),
     POINT_UPDATE_FAIL(409, "Point update failed."),
     SERVER_ERROR(500, "Server Error");
 
